a0c5562393d28a01980898d450e177d86a17f424
Sven <continentcraft4@gmail.com>
2022-10-13 10:08:52 -0600

    Added dog contestants

---
+++ src/components/Components.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Components = () => {
+    return (
+      <div id="Components">
+          <div id="dog"> Penny </div>
+          <div id="dog"> Denver </div>
+          <div id="dog"> Rocko </div>
+      
+    </div>
+    );
+  };
+
+export default Components;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import {Navbar} from './';
+import {Components} from './';
 
 const Main = () => {
   return (
-    <div id="navbar">
-      <h2>The Puppy Bowl </h2>
-      <Navbar />
-    
-  </div>
+    <div id="main">
+      <div id="navbar">
+        <h2>The Puppy Bowl </h2>
+        <Navbar />
+      </div>
+      <div id="components">
+        <Components />
+      </div>
+    </div>
   );
 };
 

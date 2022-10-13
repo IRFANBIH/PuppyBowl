8fadfc1e2e1d15a92bbf00f5325adaacafeb5090
irfan <ipekusic@yahoo.com>
2022-10-13 13:44:25 -0400

    added map to compenents jsx

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
-const Components = () => {
+const Components = (props) => {
+  const myDogs = props.dogData
     return (
-      <div id="Components">
-          <div id="dog"> Penny </div>
-          <div id="dog"> Denver </div>
-          <div id="dog"> Rocko </div>
-      
+      <div className ='box'>{}
+      {myDogs.map((dogs) => {
+        return(
+          <div key = {`player-${puppy.id}`} className='dog'>
+            <div>{dogs.name}</div>
+            <div>{dogs.breed}</div>
+            <div>{dogs.status}</div>
+          </div>
+        )
+      })}
     </div>
+      
     );
   };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Navbar} from './';
 import {Components} from './';
 
 const Main = () => {
+  const [dogData, setDogData] = useState([]);
+  useEffect(() => {
+    async function getDogData(){
+      const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-FT/players')
+      const result = await response.json()
+      const dogs = result.data.players
+      setDogData(dogs)
+    }
+    getDogData()
+  },[])
   return (
     <div id="main">
       <div id="navbar">
         <h2>The Puppy Bowl </h2>
         <Navbar />
-      </div>
-      <div id="components">
-        <Components />
+        {/* <Components const getDogs = {props} /> */}
       </div>
     </div>
   );

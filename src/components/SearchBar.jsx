59de7f371fd43debf42536160463d1f6130a5c51
irfan <ipekusic@yahoo.com>
2022-10-16 11:36:26 -0400

    added an on change, but still cant reach the puppies... I tried

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,26 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Dogs from "./Dogs";
+import { Main } from "./Main";
 
-const SearchBar = () =>{
-    const [searchData, setSearchData] = useState();
-    const updateSearch = (e) => {
-        setSearchData(e.target.value)
-    }
- 
-    return (
-        <div id='searchBar'>
-            <input type="text" placeholder="Search Puppies ..." onChange={updateSearch} />
-            
-        </div>
-    )
-}
+const SearchBar = () => {
+  const [searchData, setSearchData] = useState();
+    // console.log(Dogs.filter(getDog=>getDog.name.includes("k")));
+    console.log(searchData)
+  
+
+  return (
+    <div id="searchBar">
+      <input
+        type="text"
+        placeholder="Search Puppies ..."
+        onChange={(event) => {
+          setSearchData(event.target.value);
+        }}
+      />
+      
+      
+    </div>
+  );
+};
 
 export default SearchBar;
